17c534aab988818260893a0413bee3abf731652e
Gimin <agnusdei0717@gmail.com>
2022-01-05 17:32:45 +0800

    GuaiBi Robot config

--- src/public/assets/js/robots/GuaiBi.js
+++ src/public/assets/js/robots/GuaiBi.js
@@ -7,43 +7,83 @@ export default class GuaiBi extends Robot {
             bulletSpeed: 10,
             faceDirection: 'up'
         })
-        this.rdnNum = 0;
         this.phase = 0;
+        this.attackType = 0;//0:side attack,1:four side  random attack
+        this.attackTimes = 0;
+        this.countSideAttack = 0;
     }
 
     // run 2 times per second
     // X:800 Y:600
     update = () => {
-        switch (this.phase) {
-            case 0:
-                this.x = 0;
-                this.y = Phaser.Math.Between(0,550);
-                this.setFaceDirection('right');
-                break;
-            case 1:
-                this.x = 750;
-                this.y = Phaser.Math.Between(0,550);
-                this.setFaceDirection('left');
-                break;
-            case 2:
-                this.x = Phaser.Math.Between(0,750);
-                this.y = 0;
-                this.setFaceDirection('down');
-                break;
-            case 3:
-                this.x = Phaser.Math.Between(0,750);
-                this.y = 550;
-                this.setFaceDirection('up');
-                break;
 
+        if (this.attackType == 0) {
+            switch (this.phase) {
+                case 0:
+                    this.x = 50;
+                    this.y = 50;
+                    this.setFaceDirection('down');
+                    break;
+                case 1:
+                    this.x = 750;
+                    this.y = 50;
+                    this.setFaceDirection('left');
+                    break;
+                case 2:
+                    this.x = 750;
+                    this.y = 550;
+                    this.setFaceDirection('up');
+                    break;
+                case 3:
+                    this.x = 50;
+                    this.y = 550;
+                    this.setFaceDirection('right');
+                    break;
 
+            }
+            this.attackTimes++;
+            if (this.attackTimes == 3) {
+                this.phase++;
+                this.attackTimes = 0;
+                if (this.phase == 4) {
+                    this.phase = 0;
+                    this.countSideAttack++;
+                }
+            }
+
+            if(this.countSideAttack == 3){
+                this.attackType = 1;
+            }
+        } else if (this.attackType == 1) {
+            switch (this.phase) {
+                case 0:
+                    this.x = 0;
+                    this.y = Phaser.Math.Between(0, 550);
+                    this.setFaceDirection('right');
+                    break;
+                case 1:
+                    this.x = 750;
+                    this.y = Phaser.Math.Between(0, 550);
+                    this.setFaceDirection('left');
+                    break;
+                case 2:
+                    this.x = Phaser.Math.Between(0, 750);
+                    this.y = 0;
+                    this.setFaceDirection('down');
+                    break;
+                case 3:
+                    this.x = Phaser.Math.Between(0, 750);
+                    this.y = 550;
+                    this.setFaceDirection('up');
+                    break;
+            }
+            this.phase++;
+            if (this.phase == 4) {
+                this.phase = 0;
+            }
         }
         this.fire();
-        // this.phase = Math.floor(Math.random() * 400) % 4;
-        this.phase++;
-        if(this.phase == 4){
-            this.phase = 0;
-        }
+
 
     }
 }
