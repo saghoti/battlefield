7ad967497a49a9c2d0ec8efbaf9b1f503a1ed661
arthur.huang <sasans8504@gmail.com>
2022-01-06 11:34:51 +0800

    popo 3.0

--- src/public/assets/js/robots/Popo.js
+++ src/public/assets/js/robots/Popo.js
@@ -5,14 +5,14 @@ export default class Popo extends Robot {
     super(scene, 750, 150, {
       name: 'popo',
       bulletSpeed: 9,
-      faceDirection: 'left',
+      faceDirection: 'down',
       sensorRadius: 150
     })
   }
 
   update = () => {
-    this.y = Phaser.Math.Between(50, 550)
-    this.fire()
+    // this.y = Phaser.Math.Between(50, 550)
+    this.clockwiseTurn(true)
 
     // if (this.invincible) {
     //   this.setFaceDirection('up')
@@ -23,7 +23,51 @@ export default class Popo extends Robot {
     // }
   }
 
-  bulletSensor = () => {
-    console.log('bullet!')
+  bulletSensor = (target) => {
+    if (Math.abs(target.x - this.x) <= 50) {
+      if (this.x > 700) {
+        this.x = 70
+      } else {
+        this.x += 70
+      }
+      if (target.y < this.y) {
+        this.setFaceDirection('down')
+      } else if (target.y > this.y) {
+        this.setFaceDirection('up')
+      }
+    }
+
+    if (Math.abs(target.y - this.y) <= 50) {
+      if (this.y > 500) {
+        this.y = 70
+      } else {
+        this.y += 70
+      }
+      if (target.x < this.x) {
+        this.setFaceDirection('left')
+      } else if (target.x > this.x) {
+        this.setFaceDirection('right')
+      } 
+    }
+  }
+
+  clockwiseTurn(shoot = false) {
+    switch(this.getFaceDirection()) {
+      case 'up':
+        this.setFaceDirection('right')
+        break
+      case 'right':
+        this.setFaceDirection('down')
+        break
+      case 'down':
+        this.setFaceDirection('left')
+        break
+      case 'left':
+        this.setFaceDirection('up')
+        break
+    }
+    if (shoot) {
+      this.fire()
+    }
   }
 }
