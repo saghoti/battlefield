ff10337039c443c8c76b07f2596262cf1ad72b59
scott.lin <corum9r833@gmail.com>
2022-01-06 11:53:34 +0800

    scottRobot V1.0.3

--- src/public/assets/js/robots/GF2014NA.js
+++ src/public/assets/js/robots/GF2014NA.js
@@ -2,44 +2,83 @@ import Robot from "./Robot.js"
 
 export default class GF2014NA extends Robot {
     constructor(scene, x, y) {
-        super(scene, 300, 100, {
+        super(scene, 35, 35, {
             name: 'GF2-014NA',
             bulletSpeed: 100,
-            faceDirection: 'down'
+            faceDirection: 'right'
         })
         this.setSensorRadius(100)
     }
 
 // run 2 times per second
+    bulletSensor = (target) => {
+
+        if (target.body != null) {
+            var bodyX = target.body.velocity.x
+            var bodyY = target.body.velocity.y
+
+            if (bodyX > 0) {
+                this.setFaceDirection("left")
+                if (this.y > 565)
+                    this.y = target.y + Phaser.Math.Between(-50, -1)
+                else
+                    this.y = target.y + Phaser.Math.Between(1, 50)
+            } else {
+                this.setFaceDirection("right")
+                if (this.y > 565)
+                    this.y = target.y + Phaser.Math.Between(-50, -1)
+                else
+                    this.y = target.y + Phaser.Math.Between(1, 50)
+            }
+
+            if(bodyY>0){
+                this.setFaceDirection("down")
+                if (this.x > 765)
+                    this.x = target.x + Phaser.Math.Between(-50, -1)
+                else
+                    this.x = target.x + Phaser.Math.Between(1, 50)
+            }else {
+                this.setFaceDirection("up")
+                if (this.x > 765)
+                    this.x = target.x + Phaser.Math.Between(-50, -1)
+                else
+                    this.x = target.x + Phaser.Math.Between(1, 50)
+            }
+
+        }
+
+    }
     update = () => {
+
+
         if (this.hasTarget()) {
             const target = this.getFirstTarget()
             console.log(target.x, target.y)
             this.y = target.y + 100
             this.x = target.x - 100
-            this.setFaceDirection("left");
+            this.setFaceDirection("right");
             this.fire()
         }
-
+        this.setFaceDirection("right");
         this.fire()
         // this.x =Phaser.Math.Between(50, 800)
         // this.y = Phaser.Math.Between(50, 550)
-        var face = Phaser.Math.Between(0, 4)
-        switch (face) {
-            case 1:
-                this.setFaceDirection("left");
-                break;
-
-            case 2:
-                this.setFaceDirection("right");
-                break;
-            case 3:
-                this.setFaceDirection("down");
-                break;
-            case 4:
-                this.setFaceDirection("up");
-                break;
-        }
+        // var face = Phaser.Math.Between(0, 4)
+        // switch (face) {
+        //     case 1:
+        //         this.setFaceDirection("left");
+        //         break;
+        //
+        //     case 2:
+        //         this.setFaceDirection("right");
+        //         break;
+        //     case 3:
+        //         this.setFaceDirection("down");
+        //         break;
+        //     case 4:
+        //         this.setFaceDirection("up");
+        //         break;
+        // }
 
 
     }
